3e39e37fe32269e0cfa1d9e383ea18b5f70ffc45
itgd <1740025585@qq.com>
2020-08-20 10:27:09 +0800

    主页功能完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -5,4 +5,23 @@ var baseURL = 'http://ajax.frontend.itheima.net'
 $.ajaxPrefilter(function (params) {
     //拼接对应的环境地址
     params.url = baseURL + params.url;
-})
+
+
+    if (params.url.indexOf("/my/") !== -1) {
+        params.headers = {
+            Authorization: localStorage.getItem("token") || ""
+        }
+    }
+    //拦截所有响应
+    params.complete = function (res) {
+        console.log(res.responseJSON);
+        var obj = res.responseJSON;
+        if (obj.status == 1 && obj.message == '身份认证失败！') {
+            //清空本地缓存
+            localStorage.removeItem("token");
+            //页面跳转
+            location.href = "/login.html"
+        }
+
+    }
+});
---
+++ assets/js/index.js
@@ -0,0 +1,44 @@
+$(function () {
+    getUserInfo();
+    //退出
+    var layer = layui.layer;
+    $('#btnLogout').on('click', function () {
+        layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function (index) {
+            //清空本地缓存
+            localStorage.removeItem("token");
+            //页面跳转
+            location.href = "/login.html"
+            //关闭询问框
+            layer.close(index);
+        });
+    })
+})
+//获取信息
+function getUserInfo() {
+    $.ajax({
+        url: '/my/userinfo',
+        // headers: {
+        //     Authorization: localStorage.getItem("token") || ""
+        // },
+        success: function (res) {
+            if (res.status !== 0) {
+                return layui.layer.msg(res.message);
+            }
+            renderAvatar(res.data)
+        }
+    })
+}
+//封装用户头像渲染函数
+function renderAvatar(user) {
+    var name = user.nickname || user.username;
+    $('#welcome').html("欢迎&nbsp;&nbsp;" + name);
+    if (user.user_pic !== null) {
+        //有头像
+        $('.layui-nav-img').show().attr("src", user.user_pic);
+        $('.user-avatar').hide();
+    } else {
+        $('.layui-nav-img').hide();
+        var text = name[0].toUpperCase();
+        $('.user-avatar').show().html(text);
+    }
+}
