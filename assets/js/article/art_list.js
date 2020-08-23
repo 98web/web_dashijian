ab1a529e0564023f555fe2c1559f67cccde20535
itgd <1740025585@qq.com>
2020-08-23 22:55:23 +0800

    文章管理补全

--- assets/js/article/art_list.js
+++ assets/js/article/art_list.js
@@ -1,9 +1,7 @@
 $(function () {
-    var form = layui.form;
-    var layer = layui.layer;
-    //添加时间过滤器
-    template.defaults.imports.dataFormat = function (date) {
-        const dt = new Date(date)
+    // 为 art-template 定义时间过滤器
+    template.defaults.imports.dateFormat = function (dtStr) {
+        var dt = new Date(dtStr)
 
         var y = dt.getFullYear()
         var m = padZero(dt.getMonth() + 1)
@@ -16,111 +14,124 @@ $(function () {
         return y + '-' + m + '-' + d + ' ' + hh + ':' + mm + ':' + ss
     }
 
-    // 定义补零的函数
+    // 在个位数的左侧填充 0
     function padZero(n) {
-        return n < 10 ? "0" + n : n;
+        return n > 9 ? n : '0' + n
     }
 
+
+    // 1.定义提交参数;
     var q = {
-        pagenum: 1, // 页码值，默认请求第一页的数据
-        pagesize: 2, // 每页显示几条数据，默认每页显示2条
-        cate_id: '', // 文章分类的 Id
-        state: '' // 文章的发布状态
-    }
-    //初始化文章列表 GET获取表格数据渲染到页面template(模板内的值,提出来的值)
+        pagenum: 1, // 页码值
+        pagesize: 2, // 每页显示多少条数据
+        cate_id: "",// 文章分类的 Id
+        state: "",// 文章的状态，可选值有：已发布、草稿
+    };
+
+    // 2.初始化文章列表
     initTable();
-    //渲染到下拉框
-    initCate();
     function initTable() {
         $.ajax({
-            method: 'GET',
+            method: 'get',
             url: '/my/article/list',
             data: q,
             success: function (res) {
-                var str = template('tpl-table', res);
-                console.log(res);
-                $('tbody').html(str);
-                //直接调用分页
-                renderPage(res.total);
-            }
-        })
-    }
-    //把数据渲染到下拉框
-    function initCate() {
-        $.ajax({
-            //获取文章分类列表
-            url: '/my/article/cates',
-            success: function (res) {
-                if (res.status !== 0) {
-                    return layer.msg('获取分类数据失败')
-                }
-                var htmlStr = template('tpl-cate', res)
-                $('[name=cate_id]').html(htmlStr)
-                form.render()
+                // console.log(res);
+                var str = template("tpl-table", res);
+                $("tbody").html(str);
+                // 分页
+                renderPage(res.total)
             }
         })
     }
 
-    //筛选绑定提交事件
-    $('#form-search').on('submit', function (e) {
+    // 3.初始化分类
+    var form = layui.form;  // 导入form
+    initCate(); // 调用函数
+    // 封装
+    function initCate() {
+        $.ajax({
+            method: "GET",
+            url: '/my/article/cates',
+            success: function (res) {
+                // 校验
+                if (res.status !== 0) {
+                    return layer.msg(res.message);
+                }
+                // 赋值，渲染form
+                var htmlStr = template("tpl-cate", res);
+                $("[name=cate_id]").html(htmlStr);
+                form.render();
+            }
+        })
+    }
+
+    // 4.筛选功能
+    $("#form-search").on("submit", function (e) {
         e.preventDefault();
-        var cate_id = $('[name=cate_id]').val()
-        var state = $('[name=state]').val()
-        //赋值
-        q.cate_id = cate_id
-        q.state = state
+        // 获取
+        var state = $("[name=state]").val();
+        var cate_id = $("[name=cate_id]").val();
+        // 赋值
+        q.state = state;
+        q.cate_id = cate_id;
+        // 初始化文章列表
         initTable();
     })
 
-    //分页功能
+    // 5.分页
     var laypage = layui.laypage;
     function renderPage(total) {
         //执行一个laypage实例
         laypage.render({
-            elem: 'test1' //注意，这里的 test1 是 ID，不用加 # 号
-            , count: total,//数据总数，从服务端得到
-            limit: q.pagesize,//每页几条
-            curr: q.pagenum,//默认选中的页面
-            //分页模块显示那些子模块
+            elem: 'pageBox', //注意，这里的 test1 是 ID，不用加 # 号
+            count: total, //数据总数，从服务端得到
+            limit: q.pagesize, // 每页几条
+            curr: q.pagenum, // 第几页
+
+            // 分页模块设置，显示哪些些子模块
             layout: ['count', 'limit', 'prev', 'page', 'next', 'skip'],
-            limits: [2, 3, 5, 7],
+            limits: [2, 3, 5, 10], // 每页显示多少条数据的选择器
 
             jump: function (obj, first) {
                 //obj包含了当前分页的所有参数，比如：
                 console.log(first, obj.curr, obj.limit); //得到当前页，以便向服务端请求对应页的数据。
-                //obj.limit得到每页显示的条数
-                //赋值
+                // 赋值页面
                 q.pagenum = obj.curr;
                 q.pagesize = obj.limit;
                 //首次不执行
                 if (!first) {
                     //do something
-                    initTable();
+                    initTable()
                 }
             }
         });
     }
 
-    //删除
-    $('tbody').on('click', '.btn-delete', function () {
-        var id = $(this).attr('data-id')
-        // 提示用户是否要删除
-        layer.confirm('确认删除?', { icon: 3, title: '提示' }, function (index) {
+    // 6.删除
+    var layer = layui.layer;
+    $("tbody").on("click", ".btn-delete", function () {
+        // ！！！ 4.1 先获取 Id ,进入到函数中this代指就改变了
+        var Id = $(this).attr("data-id");
+        // 4.1 显示对话框
+        layer.confirm('是否确认删除?', { icon: 3, title: '提示' }, function (index) {
+            //do something
             $.ajax({
                 method: 'GET',
-                url: '/my/article/delete/' + id,
+                url: '/my/article/delete/' + Id,
                 success: function (res) {
                     if (res.status !== 0) {
                         return layer.msg(res.message)
                     }
-                    layer.msg('恭喜你，删除文章成功！')
-                    if ($('.btn-delete').length == 1 && q.pagenum > 1) q.pagenum--;
+                    layer.msg("恭喜您，文章删除成功！");
+                    // 页面汇总删除按钮个数等于1，页码大于1；
+                    if ($(".btn-delete").length == 1 && q.pagenum > 1) q.pagenum--;
+                    // if ($("tbody tr").length == 1 && q.pagenum > 1) q.pagenum--;
+                    // 因为我们更新成功了，所以要重新渲染页面中的数据
                     initTable();
                 }
             })
-            layer.close(index)
-        })
+            layer.close(index);
+        });
     })
-
-
-});
+})
